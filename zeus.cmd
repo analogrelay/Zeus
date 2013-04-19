48d22483efd38d5042e36ac9eb2fa922d8eb6be2
anurse <andrew@andrewnurse.net>
2013-04-18 23:17:58 -0700

    Added command to launch zeus from home directory

--- zeus.cmd
+++ zeus.cmd
@@ -1,6 +1,6 @@
 @echo off
 if exist %~dp0src\Zeus\bin\Debug\Zeus.exe goto run
-echo "Zeus has not been built yet!"
+echo Zeus has not been built yet!
 exit /B 1
 :run
 %~dp0src\Zeus\bin\Debug\Zeus.exe %*
