82d6a7a03484437b749509cce8502c590d612f41
anurse <andrew@andrewnurse.net>
2013-05-31 22:59:36 -0700

    Guh.

---
+++ Commander.Facts/TokenizerFacts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Commander.Facts
+{
+    public class TokenizerFacts
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void GivenNoSwitchPrefixes_ItShouldInitializeTheSwitchPrefixesProperty()
+            {
+                // Act
+                var tokenizer = new Tokenizer();
+
+                // Assert
+                Assert.Equal(new[] { "--", "-", "/" }, tokenizer.SwitchPrefixes);
+            }
+
+            [Fact]
+            public void GivenSwitchPrefixes_ItShouldInitializeTheSwitchPrefixesProperty()
+            {
+                string[] expected = new [] { "!", "?", "$" };
+
+                // Act
+                var tokenizer = new Tokenizer(expected);
+
+                // Assert
+                Assert.Equal(expected, tokenizer.SwitchPrefixes);
+            }
+        }
+
+        public class Tokenize
+        {
+            [Fact]
+            public void GivenNullArray_ItShouldThrowArgNull()
+            {
+                var ex = Assert.Throws<ArgumentNullException>(() => new Tokenizer().Tokenize(null));
+                Assert.Equal("args", ex.ParamName);
+            }
+
+            [Fact]
+            public void GivenEmptyArray_ItShouldReturnEmptyEnumerable()
+            {
+                Assert.Empty(new Tokenizer().Tokenize(new string[0]));
+            }
+        }
+    }
+}
---
+++ Commander/Tokenizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commander
+{
+    public class Tokenizer
+    {
+        private static readonly string[] _defaultSwitchPrefixes = new[] {
+            "--",
+            "-",
+            "/"
+        };
+
+        public IEnumerable<string> SwitchPrefixes { get; private set; }
+
+        public Tokenizer() : this(_defaultSwitchPrefixes) { }
+
+        public Tokenizer(params string[] switchPrefixes)
+        {
+            SwitchPrefixes = switchPrefixes;
+        }
+
+        public IEnumerable<object> Tokenize(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException("args");
+            }
+
+            // Call an iterator method to do the actual parsing.
+            return TokenizeCore(args);
+        }
+
+        
+        private IEnumerable<object> TokenizeCore(string[] args)
+        {
+            yield break;
+        }
+    }
+}
